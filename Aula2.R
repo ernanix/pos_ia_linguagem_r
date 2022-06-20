3ddf5454cb23d1a5853238a0d968afec291d7548
ERNANI DE SOUZA CUBAS NETO <escneto@ad.mppr.mp.br>
2022-06-20 18:11:20 -0300

    tarefas cpu trabalho

--- Aula2.R
+++ Aula2.R
@@ -80,3 +80,59 @@ fert<-read.csv("fertility.csv")
 write.table(iris,"iris1.txt",sep="**",dec=".",row.names=FALSE,col.names = FALSE)
   #b.CSV com ponto decimal ",", sem os nomes das linhas, com o cabeçalho de colunas e sem aspas nos campos string
 write.table(iris,"iris1.csv",sep=";",dec=",",row.names=FALSE,col.names = TRUE,quote = FALSE)
+
+
+#Slide 11 - Página 20
+#Escreva um laço que varre os números de 1 a 7 e imprime seus quadrados, usando o comando print()
+vetor <- 1:7
+for (i in vetor) {
+  print(i^2)
+}
+
+vetor2 <- rnorm(10)
+#Usando laços, varra uma lista de números aleatórios gerados por rnorm(), mas pare se o número encontrado for mais que 1.
+for (i in vetor2) {
+  if (i > 1)
+    break
+  else
+    print(i)
+}
+
+#Usando laços, varra uma lista de números aleatórios gerados por rnorm(), mas use o comando next para pular os números negativos
+i<-1
+while (i <= length(vetor2)) {
+  if (vetor2[i] < 0) {
+    i <- i+1
+    next
+  }
+  print(vetor2[i])
+  i <- i+1
+}
+
+#Use laços aninhados para criar a matriz abaixo. Faça a alocação prévia da matriz com valores NA
+matriz <-matrix(data=NA,nrow=5,ncol=5)
+
+for (l in 1:nrow(matriz)) {
+  for (c in 1:ncol(matriz)) {
+    if (l == c) {
+      matriz[l,c] <- 0
+    }
+    if (l==c-1 || l-1==c){
+      matriz[l,c] <- 1
+    }
+    if (l==c-2 || l-2==c){
+      matriz[l,c] <- 2
+    }
+    if (l==c-3 || l-3==c){
+      matriz[l,c] <- 3
+    }
+    if (l==c-4 || l-4==c){
+      matriz[l,c] <- 4
+    }
+  }
+}
+
+#Crie o seguinte data frame
+student.df <- data.frame( name = c("Sue", "Eva", "Henry", "Jan"),sex = c("f", "f", "m", "m"),years = c(21,31,29,19));
+
+#Usando um comando ifelse(), crie uma coluna chamada teen, booleana, que indica se a pessoa possui menos de 20 anos.
